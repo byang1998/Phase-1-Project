288ed1734fd1cbb30136f7397e15595b3e2a5901
Ben Yang <benyang2020@gmail.com>
2021-09-14 12:30:52 -0400

    fourth commit

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ const BASE_URL = "http://localhost:3000/cities"
 
 fetch(BASE_URL)
 .then(resp => resp.json())
-.then(data => console.log("This is a check."))
+.then(function(cityData){
+    cityData.forEach((cities) => renderCities(cities));
+    })
+function renderCities(cities){
+    const cityImg = document.createElement("img");
+    cityImg.src = cities.img;
+    cityImg.alt = `${cities.name} image`;
+    console.log(cities)
+    
+
+}
