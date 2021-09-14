e29b1efbcea46b1068dfe67b8a913a806408f850
Jon Courtier <jonaqwerty@yahoo.com>
2021-09-14 19:21:18 -0400

    render commit

--- index.js
+++ index.js
@@ -5,12 +5,32 @@ fetch(BASE_URL)
 .then(function(cityData){
     cityData.forEach((cities) => renderCities(cities));
     })
-    
+
 function renderCities(cities){
-    const cityImg = document.createElement("img");
+    const citiesDiv = document.createElement('div')
+    citiesDiv.className = "card"
+
+    const citiesName = document.createElement('h3')
+    citiesName.innerText = cities.name
+
+    const cityImg = document.createElement("img")
     cityImg.src = cities.img;
     cityImg.alt = `${cities.name} image`;
-    console.log(cities)
+
+    const cityPop = document.createElement('h4')
+    cityPop.innerText = cities.population
+
+    const cityRating = document.createElement('p')
+    cityRating.innerText = "Rating: " + cities.rating
+
+    const voteBttn = document.createElement('button')
+    voteBttn.innerText = "Vote"
+    voteBttn.addEventListener('click', () => {
+        cities.rating++
+        cityRating.innerText = "Vote: " + cities.rating
+    })
+
+citiesDiv.append(cityName, cityImg, cityRating, voteBttn)
     
 
 }
