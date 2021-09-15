4baa5ffb478a80d0772e55f25097e17fa56e6d0a
Jon Courtier <jonaqwerty@yahoo.com>
2021-09-15 11:37:31 -0400

    Submit Function

--- index.js
+++ index.js
@@ -23,8 +23,12 @@ function renderCities(cities){
     const cityPop = document.createElement('h4')
     cityPop.innerText = "Population: " + cities.population
 
+    const cityState = document.createElement('p')
+    cityState.innerText = "State: " + cities.state
+
     const cityRating = document.createElement('p')
     cityRating.innerText = "Rating: " + cities.rating
+
 
     const voteBttn = document.createElement('button')
     voteBttn.innerText = "Vote"
@@ -40,12 +44,28 @@ function renderCities(cities){
 
 function deleteCity(card){
     citiesDiv.remove()
-    
-    console.log(citiesDiv)
-}
+    }
 
 
-citiesDiv.append(citiesName, cityImg, cityPop, cityRating, voteBttn, deleteBttn)
+citiesDiv.append(citiesName, cityImg, cityPop, cityState, cityRating, voteBttn, deleteBttn)
 cityContainer.append(citiesDiv)
 
+}
+
+const newCityForm = document.getElementById('cities-form')
+newCityForm.addEventListener('submit', submitHandler)
+
+function submitHandler(e){
+    e.preventDefault()
+
+const newCity = {
+    name: e.target["name"].value,
+    population: e.target["population"].value,
+    image: e.target["image"].value,
+    state: e.target["state"].value,
+    rating: +e.target["rating"].value,
+}
+
+renderCities(newCity)
+e.target.reset()
 }
